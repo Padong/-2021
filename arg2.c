91dfdb34f58063c89b50d5aa7b48a6ee75f6e437
Padong <nyc80886@naver.com>
2021-12-11 17:20:24 +0900

    매개변수에서 포인터

--- arg2.c
+++ arg2.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-	FILE *fp = NULL;
+	FILE *f = NULL;
 	int i = 35421678;
 	
-	fp = fopen("table1.txt", "w+b");
-	fseek(fp, 100, SEEK_SET);
-	fwrite(&i,sizeof(int), 1, fp);
-	fclose(fp);
+	f = fopen("table1.txt", "w+b");
+	fseek(f, 100, SEEK_SET);
+	fwrite(&i,sizeof(int), 1, f);
+	fclose(f);
 }
 
